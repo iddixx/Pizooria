c39671c9b50522d4e249a288f43e100211e08559
iddixx <ondarmurad2009@gmail.com>
2025-08-14 19:52:27 +0300

    Created Scriptable object for pizza

---
+++ Pizooria/Assets/Scripts/PizzaObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "PizzaObject", menuName = "Pizooria/IngredientObject")]
+public class PizzaObject : IngredientObject 
+{
+    public float BakingTime; // In seconds
+}
+
+
