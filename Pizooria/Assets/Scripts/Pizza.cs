80be2a2eaadd7e631ec518e80b2ca3dcb87f4e24
iddixx <ondarmurad2009@gmail.com>
2025-08-31 03:43:58 +0300

    Pizza.cs: Made pizzas don't stack over 1

--- Pizooria/Assets/Scripts/Pizza.cs
+++ Pizooria/Assets/Scripts/Pizza.cs
@@ -9,7 +9,13 @@ public class Pizza : Ingredient
     public bool IsBaked { get; private set; }
     override protected void Start()
     {
+        if(MaxStack > 1)
+        {
+            Debug.LogWarning($"MaxStack in Pizza can't be greater than 1, setting to 1", this);
+            MaxStack = 1;
+        }
         base.Start();
+
         if(ScriptableObject is PizzaObject pizza_so)
         {
             BakingTime = pizza_so.BakingTime;
