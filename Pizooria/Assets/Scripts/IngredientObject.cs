02523603096edb9e4a7fce6cca1100c69143e06a
iddixx <ondarmurad2009@gmail.com>
2025-09-05 01:31:49 +0300

    IngredientObject.cs: Replaced placeholder with CraftUnit
    Replaced 'enum CraftType' with CraftUnit structure, now
    IngredientObject.Craft is CraftUnit array

--- Pizooria/Assets/Scripts/IngredientObject.cs
+++ Pizooria/Assets/Scripts/IngredientObject.cs
@@ -2,10 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[System.Serializable]
-public enum CraftType
+public struct CraftUnit
 {
-    NOT_READY
+    public IngredientObject Ingredient;
+    public uint Count;
+    public CraftUnit(IngredientObject ingredient, uint count)
+    {
+        if(count == 0) throw new System.ArgumentException("You shouldn't assign 0 to Count in CraftUnit");
+        Ingredient = ingredient;
+        Count = count;
+    }
 }
 
 [CreateAssetMenu(fileName = "IngredientObject", menuName = "Pizooria/IngredientObject")]
@@ -15,6 +21,6 @@ public class IngredientObject : ScriptableObject
     public int Cost;
     public int AmmountPerCost; // i know that Ammount is incorrect, but everything breaks when i change it
     public Sprite SelfSprite;
-    public CraftType Craft;
+    public CraftUnit[] Craft = System.Array.Empty<CraftUnit>();
 }
 
