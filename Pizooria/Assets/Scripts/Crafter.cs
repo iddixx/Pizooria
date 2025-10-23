7401ecefaa85f25953d2451a41e7e793c7ce9d9a
iddixx <ondarmurad2009@gmail.com>
2025-10-23 20:10:01 +0300

    Crafter.cs: Fixed merge conflict

--- Pizooria/Assets/Scripts/Crafter.cs
+++ Pizooria/Assets/Scripts/Crafter.cs
@@ -21,7 +21,6 @@ static public class Crafter
     // returns null if there's no craft
     static public CraftUnit? Craft(CraftUnit[] craft)
     {
-<<<<<<< HEAD
         if((craft.Length == 0) || (craft == null)) return null;
 
         IngredientObject? craft_result = IngredientCatalogue.instance.Find(obj => CraftEquals(craft, obj.Craft));
@@ -30,15 +29,6 @@ static public class Crafter
             return new CraftUnit(craft_result, (uint)craft_result.AmmountPerCost);
         }
         return null;
-=======
-        throw new Exception();
-        //IngredientObject? craft_result = IngredientCatalogue.instance.Find(obj => CraftEquals(craft, obj.Craft));
-        //if(craft_result != null)
-        ///
-           // return new CraftUnit(craft_result, (uint)craft_result.AmmountPerCost);
-        //}
-        //return null;
->>>>>>> master
     }
 #nullable disable
 }
