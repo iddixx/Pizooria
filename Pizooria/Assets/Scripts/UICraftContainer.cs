a882af96f11c46b05eb0f9b75b5ae48392afc8c7
iddixx <ondarmurad2009@gmail.com>
2025-11-04 00:54:51 +0200

    Added UICraftContainer, name is self-explanatory

---
+++ Pizooria/Assets/Scripts/UICraftContainer.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine;
+
+[RequireComponent(typeof(VerticalLayoutGroup))] // DO NOT CHANGE THIS LINE
+public class UICraftContainer : MonoBehaviour
+{
+    public UICraftView CraftPrefab;
+    private List<UICraftView> _contents;
+
+    private void OnEnable()
+    {
+        FridgeManager.Instance.BoughtIngredient.AddListener(UpdateContents);
+        FridgeManager.Instance.DecreasedIngredient.AddListener(UpdateContents);
+    }
+
+    private void OnDisable()
+    {
+        FridgeManager.Instance.BoughtIngredient.RemoveListener(UpdateContents);
+        FridgeManager.Instance.DecreasedIngredient.RemoveListener(UpdateContents);
+    }
+
+    private void ClearContents()
+    {
+        while(_contents.Count != 0)
+        {
+            Destroy(_contents[0].gameObject);
+            _contents.RemoveAt(0);
+        }
+    }
+
+    private void UpdateContents()
+    {
+        ClearContents();
+        foreach(IngredientObject obj in IngredientCatalogue.instance.Ingredients)
+        {
+            if(Crafter.CanCraft(obj))
+            {
+                UICraftView craft = Instantiate(CraftPrefab, this.transform);
+                craft.SetCraft(obj);
+                _contents.Add(craft);
+            }
+        }
+    }
+}
